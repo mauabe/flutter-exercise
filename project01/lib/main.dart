56fa513702a645b223444b11f000488ce74c1efd
Mauricio <mauabe@gmx.com>
2019-04-14 18:45:17 -0700

    created state widget

--- project01/lib/main.dart
+++ project01/lib/main.dart
@@ -1,17 +1,7 @@
 import 'package:flutter/material.dart';
 // color picker in hes: http://uicolorpicker.com/
+import 'src/app.dart';
 
 void main(){
-
-  final barColor = const Color(0xFFEEC213);
-  final bgColor = const Color(0xFFEAF0F1);
-  var app = MaterialApp(
-    home: Scaffold(
-      backgroundColor: bgColor,
-      appBar: AppBar(
-        backgroundColor: barColor,
-      ),
-    ),
-  );  
-  runApp(app);
+  runApp(App());
 }
